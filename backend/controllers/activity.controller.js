5907655b224308a7a9aed4af217bc13630e1b01a
akshat-shaw-509 <akshatshaw99@gmail.com>
2026-01-10 13:44:08 +0530

    Create activity.controller.js

---
+++ backend/controllers/activity.controller.js
@@ -0,0 +1,64 @@
+let activityService = require('../services/activity.service');
+
+let sendSuccess=(res,statusCode,data=null,message=null,extra={})=>{
+  let response={success:true}
+  if(data) response.data=data
+  if(message) response.message=message
+  Object.assign(response,extra)
+  res.status(statusCode).json(response)
+}
+
+let sendError=(res,statusCode,message)=>{
+  res.status(statusCode).json({ success:false,message})
+}
+
+let createActivity = async (req, res, next) => {
+  let activity=await activityService.createActivity(req.params.tripId,req.body,req.user.id)
+  sendSuccess(res,201,activity,'Activity created successfully')
+}
+
+let getActivitiesByTrip = async (req, res, next) => {
+  let activities=await activityService.getActivitiesByTrip(req.params.tripId,req.user.id)
+  sendSuccess(res,200,activities,null,{count:activities.length}) 
+}
+
+let getActivityById = async (req, res, next) => {
+  let activity=await activityService.getActivityById(req.params.activityId,req.user.id)
+  sendSuccess(res,200,activity)
+}
+
+let updateActivity = async (req, res, next) => {
+  let activity=await activityService.updateActivity(req.params.activityId,req.body,req.user.id)
+  sendSuccess(res,200,activity,'Activity updated successfully')
+}
+
+const deleteActivity = async (req, res, next) => {
+  let result=await activityService.deleteActivity(req.params.activityId,req.user.id)
+  sendSuccess(res,200,null,result.message) 
+}
+
+let getActivitiesByDate = async (req, res, next) => {
+  if(!req.query.date){
+    return sendError(res,400,'Date query parameter required')
+  }
+  let activities=await activityService.getActivitiesByDate(req.params.tripId,req.query.date,req.user.id)
+  sendSuccess(res,200,activities,null,{count:activities.length})
+}
+
+let updateActivityStatus = async (req, res, next) => {
+  if(!req.body.status){
+    return sendError(res,400,'Status required')
+  }
+  let activity=await activityService.updateActivityStatus(req.params.activityId,req.body.status,req.user.id)
+  sendSuccess(res,200,activity,'Activity status updated successfully')
+}
+
+module.exports = {
+  createActivity,
+  getActivitiesByTrip,
+  getActivityById,
+  updateActivity,
+  deleteActivity,
+  getActivitiesByDate,
+  updateActivityStatus,
+};
