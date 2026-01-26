ea1e7a4acbe822f2423bd9e6767e6583aad53649
akshat-shaw-509 <akshatshaw99@gmail.com>
2026-01-27 02:28:00 +0530

    Add files via upload

--- docs/js/config.js
+++ docs/js/config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // frontend/js/config.js
 const API_BASE_URL = 'http://localhost:5000/api'
 const APP_NAME = 'Planora'
@@ -6,13 +5,4 @@ const APP_NAME = 'Planora'
 window.config = {
     API_BASE_URL,
     APP_NAME
-=======
-// frontend/js/config.js
-const API_BASE_URL = 'http://localhost:5000/api'
-const APP_NAME = 'Planora'
-
-window.config = {
-    API_BASE_URL,
-    APP_NAME
->>>>>>> 0007615a5a7317e8689fc9727726ee86c5585786
 }
