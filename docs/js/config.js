44dd74a9fbd3bb4991cf44baf06a4beb72bb11a8
akshat-shaw-509 <akshatshaw99@gmail.com>
2026-01-27 02:48:27 +0530

    Update API_BASE_URL for production and development

--- docs/js/config.js
+++ docs/js/config.js
@@ -1,5 +1,7 @@
 // frontend/js/config.js
-const API_BASE_URL = 'http://localhost:5000/api'
+const API_BASE_URL = window.location.hostname.includes('github.io')
+  ? 'https://trip-planner-vwar.onrender.com/api'
+  : 'http://localhost:5000/api'
 const APP_NAME = 'Planora'
 
 window.config = {
