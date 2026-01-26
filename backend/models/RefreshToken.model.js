819aa4f0f1a68acc0a1ae982fbe73e13a1a10a57
akshat-shaw-509 <akshatshaw99@gmail.com>
2026-01-27 01:26:00 +0530

    Add files via upload

--- backend/models/RefreshToken.model.js
+++ backend/models/RefreshToken.model.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // models/RefreshToken.model.js
 let mongoose = require('mongoose')
 
@@ -8,17 +7,4 @@ let refreshTokenSchema = new mongoose.Schema({
   expiresAt: { type: Date, required: true },
   createdAt: { type: Date, default: Date.now, expires: 30 * 86400 }
 })
-
-=======
-// models/RefreshToken.model.js
-let mongoose = require('mongoose')
-
-let refreshTokenSchema = new mongoose.Schema({
-  token: { type: String, required: true, unique: true },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  expiresAt: { type: Date, required: true },
-  createdAt: { type: Date, default: Date.now, expires: 30 * 86400 }
-})
-
->>>>>>> 0007615a5a7317e8689fc9727726ee86c5585786
 module.exports = mongoose.model('RefreshToken', refreshTokenSchema)
