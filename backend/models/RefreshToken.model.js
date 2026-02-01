cf42e4471b8dcb5eb8918d08f489429081a19a75
akshat-shaw-509 <akshatshaw99@gmail.com>
2026-02-01 23:25:38 +0530

    Update RefreshToken.model.js

--- backend/models/RefreshToken.model.js
+++ backend/models/RefreshToken.model.js
@@ -1,6 +1,4 @@
-// models/RefreshToken.model.js
-let mongoose = require('mongoose')
-
+const mongoose = require('mongoose')
 let refreshTokenSchema = new mongoose.Schema({
   token: { type: String, required: true, unique: true },
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
