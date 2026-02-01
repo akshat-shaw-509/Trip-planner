6f0a8e747e9aa8cc809859ce1994c05121575c54
akshat-shaw-509 <akshatshaw99@gmail.com>
2026-02-01 23:12:58 +0530

    Update AuditLog.model.js

--- backend/models/AuditLog.model.js
+++ backend/models/AuditLog.model.js
@@ -1,11 +1,10 @@
-
-// models/AuditLog.model.js
-let mongoose = require('mongoose')
+const mongoose = require('mongoose')
 
 let auditLogSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  action: { type: String, required: true }, // LOGIN_SUCCESS, PASSWORD_RESET, etc.
+  action: { type: String, required: true }, 
   ip: String,
   userAgent: String,
   details: mongoose.Schema.Types.Mixed,
 }, { timestamps: true })
+
