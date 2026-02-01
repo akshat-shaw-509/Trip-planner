26467e57b35a47f7795923add436e643bc4de8dc
akshat-shaw-509 <akshatshaw99@gmail.com>
2026-02-01 13:13:34 +0530

    Update config.js

--- docs/js/config.js
+++ docs/js/config.js
@@ -1,15 +1,10 @@
 const API_BASE_URL = window.location.hostname.includes('github.io')
   ? 'https://trip-planner-5uys.onrender.com/api'
   : 'http://localhost:5000/api';
-
 const APP_NAME = 'Planora';
-
 const GEOAPIFY_API_KEY = '133144445c81412f85c94c986b2c1831';
-
 window.CONFIG = {
   API_BASE_URL,
   APP_NAME,
   GEOAPIFY_API_KEY
 };
-
-console.log('Config loaded:', window.CONFIG);
