c2201206e3986b233da4d6f809e72bef4d53c777
akshat-shaw-509 <akshatshaw99@gmail.com>
2026-02-01 19:36:13 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,15 +1,14 @@
-let mongoose = require('mongoose')
+const mongoose = require('mongoose')
 let isConnected=false
 
 let connectDB = async () => {
+  if(isConnected) return
   try {
-    if(isConnected) return
     await mongoose.connect(process.env.MONGODB_URI)
     isConnected=true
     console.log(`MongoDB Connected: ${mongoose.connection.host}`)
-
-    process.once('SIGINT',closeConnection)
-    process.once('SIGTERM',closeConnection)
+    process.on('SIGINT',closeConnection)
+    process.on('SIGTERM',closeConnection)
     }catch(error){
       console.error('MongoDB connection error:', error.message)
       process.exit(1)
