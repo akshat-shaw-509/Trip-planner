cf1dfb53591ed2b2749712313d44c13687ed96f3
akshat-shaw-509 <akshatshaw99@gmail.com>
2026-02-04 20:03:09 +0530

    Update error.middleware.js

--- backend/middleware/error.middleware.js
+++ backend/middleware/error.middleware.js
@@ -64,7 +64,7 @@ const errorHandler = (err, req, res, next) => {
 
 // 404 handler
 const notFoundHandler = (req, res, next) => {
-  const error = new Error`Not Found - ${req.originalUrl}`);  // ← SYNTAX ERROR!
+  const error = new Error(`Not Found - ${req.originalUrl}`);  // ← SYNTAX ERROR!
   res.status(404);
   next(error);
 };
@@ -74,4 +74,5 @@ module.exports = {
   errorHandler,
   notFoundHandler
 };
+
 
